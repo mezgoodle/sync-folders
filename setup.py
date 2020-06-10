6309ec0d2408c8f86739a97e50e135371c0ce8e5
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-10 11:17:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='sync-folders',
     packages = ['sync_folders'],
     version = '0.1.5',
     license='MIT',
     description = 'Library for synchronization two folders',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author = 'Maxim Zavalniuk',
     author_email = 'mezgoodle@gmail.com',
     url = 'https://github.com/mezgoodle/sync-folders',
