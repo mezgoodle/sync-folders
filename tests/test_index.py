d88f47a7539c58fb9b31e9b4aaf8f3e611c7221b
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-17 21:47:06 +0300

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -2,5 +2,5 @@ from sync_folders import main
 
 
 def test_creation():
-    assert test_create_file('./').read_text() == CONTENT
+    assert main.test_create_file('./').read_text() == CONTENT
     assert len(list('./'.iterdir())) == 1
