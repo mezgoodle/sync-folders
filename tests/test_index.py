3e01a195b5cefc4e1b068d8751971de81301ba4e
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-17 21:46:04 +0300

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,5 +1,6 @@
 from sync_folders import main
 
 
-def test_answer():
-    assert main.summa(2, 3) == 5
+def test_creation():
+    assert test_create_file('./').read_text() == CONTENT
+    assert len(list('./'.iterdir())) == 1
