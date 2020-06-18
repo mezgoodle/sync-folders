e2ea1d7a423834a37e39a2ac320374d17977e6d3
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-18 10:33:35 +0300

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,5 +1,8 @@
 from sync_folders import main
+from util import consts
 
 
 def test_reading():
-    assert main.read_file('./tests/text.txt') == 'text for the test'
+    assert main.read_file(consts.TEST_PATH) == 'text for the test'
+    main.write_file(consts.TEST_PATH)
+    assert main.read_file(consts.TEST_PATH) == 'some data to be written to the file'
