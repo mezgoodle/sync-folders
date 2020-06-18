ed7be1dbf1a8e6a1a2604582e39825d39e11052b
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-18 10:49:06 +0300

    Fix problem

--- util/consts.py
+++ util/consts.py
@@ -1,8 +1,8 @@
 TEST_PATH = './tests/text.txt'
 TEST_DIR_PATH = './'
-TEST_DIR_LIST = ['.git', '.github',
-                 '.gitignore', '.venv',
-                 'LICENSE', 'README.md',
-                 'requirements.txt', 'setup.cfg',
-                 'setup.py', 'sync_folders',
-                 'tests', 'util', '_config.yml']
+TEST_DIR_LIST = ['.github', '.gitignore',
+                 '.venv', 'LICENSE',
+                 'README.md', 'requirements.txt',
+                 'setup.cfg', 'setup.py',
+                 'sync_folders', 'tests',
+                 'util', '_config.yml']
