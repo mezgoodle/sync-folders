6475ebfde0113a1754954d2e83c4e27c04d7cdb4
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-18 10:26:51 +0300

    Create new function

--- sync_folders/main.py
+++ sync_folders/main.py
@@ -1,6 +1,4 @@
-def test_create_file(tmp_path):
-    d = tmp_path / "sub"
-    d.mkdir()
-    p = d / "hello.txt"
-    p.write_text("CONTENT")
-    return p
+def read_file(path):
+    with open(path, 'r') as f:
+        data = f.read()
+    return data
