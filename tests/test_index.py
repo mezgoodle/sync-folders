0b1fe9899e421c213bbcd05ced6f9b064497a314
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-18 10:26:57 +0300

    Create new tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,6 +1,5 @@
 from sync_folders import main
 
 
-def test_creation():
-    assert main.test_create_file(PosixPath('PYTEST_TMPDIR/test_create_file0')).read_text() == "CONTENT"
-    assert len(list(PosixPath('PYTEST_TMPDIR/test_create_file0').iterdir())) == 1
+def test_reading():
+    assert main.read_file('./tests/text.txt') == 'text for the test'
