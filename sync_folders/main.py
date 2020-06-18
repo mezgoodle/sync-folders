81c8efe9b2b57ae44dd453c4e6094648d2ec7d78
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-18 10:32:46 +0300

    Create function for writing in to files

--- sync_folders/main.py
+++ sync_folders/main.py
@@ -2,3 +2,9 @@ def read_file(path):
     with open(path, 'r') as f:
         data = f.read()
     return data
+
+
+def write_file(path):
+    with open(path, 'w') as f:
+        data = 'some data to be written to the file'
+        f.write(data)
