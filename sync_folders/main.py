6aeaa1bff40d27f62784b1f56d2063ead901ff82
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-18 15:18:34 +0300

    Change some functions and add new one

--- sync_folders/main.py
+++ sync_folders/main.py
@@ -1,4 +1,11 @@
+from datetime import datetime
 import os
+
+
+def convert_date(timestamp):
+    d = datetime.utcfromtimestamp(timestamp)
+    formated_date = d.strftime('%d %b %Y, %H %M')
+    return formated_date
 
 
 def read_file(path):
@@ -13,13 +20,25 @@ def write_file(path, data):
 
 
 def list_dir(path):
-    entries = os.listdir(path)
-    return entries
-
-
-def files_in_dir(path):
-    files = []
+    dirs = []
     for entry in os.listdir(path):
-        if os.path.isfile(os.path.join(path, entry)):
-            files.append(entry)
+        if os.path.isdir(os.path.join(path, entry)):
+            dirs.append(entry)
+    return dirs
+
+
+def get_files(path):
+    files = []
+    dir_entries = os.scandir(path)
+    for entry in dir_entries:
+        if entry.is_file():
+            info = entry.stat()
+            files.append({
+                'name': entry.name,
+                'date': info.st_mtime,
+                'date_str': convert_date(info.st_mtime),
+            })
     return files
+
+
+print(get_files('./')[0])
