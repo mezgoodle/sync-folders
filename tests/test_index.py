d4bfbf32846be0fb7d9ec0db2de1af1c9946fdf8
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-18 10:47:11 +0300

    Create new tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -5,4 +5,9 @@ from util import consts
 def test_reading():
     assert main.read_file(consts.TEST_PATH) == 'text for the test'
     main.write_file(consts.TEST_PATH)
-    assert main.read_file(consts.TEST_PATH) == 'some data to be written to the file'
+    assert main.read_file(
+        consts.TEST_PATH) == 'some data to be written to the file'
+
+
+def test_dir():
+    assert main.list_dir(consts.TEST_DIR_PATH) == consts.TEST_DIR_LIST
--- util/consts.py
+++ util/consts.py
@@ -1,1 +1,8 @@
 TEST_PATH = './tests/text.txt'
+TEST_DIR_PATH = './'
+TEST_DIR_LIST = ['.git', '.github',
+                 '.gitignore', '.venv',
+                 'LICENSE', 'README.md',
+                 'requirements.txt', 'setup.cfg',
+                 'setup.py', 'sync_folders',
+                 'tests', 'util', '_config.yml']
