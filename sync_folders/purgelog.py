96b4f90b61dac9cd9e8d163d9505d620b288d382
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-20 12:16:50 +0300

    Update purgelog.py

--- sync_folders/purgelog.py
+++ sync_folders/purgelog.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 import shutil
 import os
@@ -10,7 +10,24 @@ if len(sys.argv) < 4:
     raise NameError('Missing arguments')
     exit(1)
 
-file_name  = sys.argv[1]
-limitize   = int(sys.argv[2])
+file_name = sys.argv[1]
+limitize = int(sys.argv[2])
 logsNumber = int(sys.argv[3])
 
+if os.path.isfile(file_name):
+    logfile_size = os.stat(file_name).st_size
+    logfile_size /= 1024
+
+    if logfile_size >= limitize:
+        if logsNumber > 0:
+            for currentFileNum in range(logsNumber, 1, -1):
+                src = str(currentFileNum - 1) + '_' + file_name
+                dst = str(currentFileNum) + '_' + file_name
+                if os.path.isfile(src):
+                    shutil.copyfile(src, dst)
+                    print(f"Copied: {src} to {dst}")
+
+            shutil.copyfile(file_name, '1_' + file_name)
+            print(f"Copied: {file_name} to 1_{file_name}")
+        myfile = open(file_name, 'w')
+        myfile.close()
