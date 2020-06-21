e0540ece7fe56a0ecf979ec35a725fe122f150b6
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-21 11:22:27 +0300

    Update purgelog.py

--- sync_folders/purgelog.py
+++ sync_folders/purgelog.py
@@ -4,7 +4,7 @@ import shutil
 import os
 import sys
 
-# purgelog.py mylog.txt 10 5
+# purgelog.py <log-file name> <limit in KB> <logs-files>
 
 if len(sys.argv) < 4:
     raise NameError('Missing arguments')
