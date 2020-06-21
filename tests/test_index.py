e2b64f8fdcba887029ade60db92cc53c589d6989
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-21 16:28:03 +0300

    Create first tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,5 +1,6 @@
-from sync_folders import main, purgelog
+from sync_folders import main, purgelog, cleaner
 from util import consts
+import os
 
 
 def test_reading():
@@ -42,3 +43,12 @@ def test_purgelog():
     assert consts.DATA == main.read_file(consts.TEST_PURGE_PATH)
     purgelog.purgelog(consts.TEST_PURGE_PATH, 5, 2)
     assert consts.DATA == main.read_file(consts.TEST_PURGE_PATH_1)
+
+
+def test_cleaner():
+    for dirname in consts.DIRS:
+        os.mkdir(dirname)
+    cleaner.cleaner([consts.DIRS[0], consts.DIRS[1], consts.DIRS[2]], consts.DAYS)
+    assert not consts.DIRS[len(consts.DIRS) -1] in main.list_dir(consts.TEST_DIR_PATH)
+    for i in range(0, 3):
+        assert not consts.DIRS[i] in main.list_dir(consts.TEST_DIR_PATH)
