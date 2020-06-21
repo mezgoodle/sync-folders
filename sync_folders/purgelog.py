93fe409a8c9f6abe72b7e3dc122feb1b3c209aef
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-21 11:40:51 +0300

    Updates

--- sync_folders/purgelog.py
+++ sync_folders/purgelog.py
@@ -4,30 +4,31 @@ import shutil
 import os
 import sys
 
-# python purgelog.py <log-file name> <limit in KB> <logs-files>
+# <log-file name> <limit in KB> <logs-files>
 
-if len(sys.argv) < 4:
-    raise NameError('Missing arguments')
-    exit(1)
+def purgelog(*args):
+    if len(args) < 3:
+        raise NameError('Missing arguments')
+        exit(1)
 
-file_name = sys.argv[1]
-limitize = int(sys.argv[2])
-logsNumber = int(sys.argv[3])
+    file_name = args[0]
+    limitize = int(args[1])
+    logsNumber = int(args[2])
 
-if os.path.isfile(file_name):
-    logfile_size = os.stat(file_name).st_size
-    logfile_size /= 1024
+    if os.path.isfile(file_name):
+        logfile_size = os.stat(file_name).st_size
+        logfile_size /= 1024
 
-    if logfile_size >= limitize:
-        if logsNumber > 0:
-            for currentFileNum in range(logsNumber, 1, -1):
-                src = str(currentFileNum - 1) + '_' + file_name
-                dst = str(currentFileNum) + '_' + file_name
-                if os.path.isfile(src):
-                    shutil.copyfile(src, dst)
-                    print(f"Copied: {src} to {dst}")
+        if logfile_size >= limitize:
+            if logsNumber > 0:
+                for currentFileNum in range(logsNumber, 1, -1):
+                    src = str(currentFileNum - 1) + '_' + file_name
+                    dst = str(currentFileNum) + '_' + file_name
+                    if os.path.isfile(src):
+                        shutil.copyfile(src, dst)
+                        print(f"Copied: {src} to {dst}")
 
-            shutil.copyfile(file_name, '1_' + file_name)
-            print(f"Copied: {file_name} to 1_{file_name}")
-        myfile = open(file_name, 'w')
-        myfile.close()
+                shutil.copyfile(file_name, '1_' + file_name)
+                print(f"Copied: {file_name} to 1_{file_name}")
+            myfile = open(file_name, 'w')
+            myfile.close()
