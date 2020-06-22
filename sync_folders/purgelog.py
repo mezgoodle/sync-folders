9fdc26c4e7eb53343b67092cf4e319f953f29eb5
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-22 09:48:00 +0300

    Add comments in purgelog.py

--- sync_folders/purgelog.py
+++ sync_folders/purgelog.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-
+# Imports
 import shutil
 import os
 
-# <log-file name> <limit in KB> <logs-files>
+"""
+List of arguments in the function
+<log-file name> <limit in KB> <logs-files>
+"""
 
 
+# Main function
 def purgelog(*args):
     if len(args) < 3:
         raise NameError('Missing arguments')
@@ -16,8 +20,8 @@ def purgelog(*args):
     logsNumber = int(args[2])
 
     if os.path.isfile(file_name):
-        logfile_size = os.stat(file_name).st_size
-        logfile_size /= 1024
+        logfile_size = os.stat(file_name).st_size   # Get size of file
+        logfile_size /= 1024                        # Get size in KB
 
         if logfile_size >= limitize:
             if logsNumber > 0:
@@ -28,7 +32,9 @@ def purgelog(*args):
                         shutil.copyfile(src, dst)
                         print(f"Copied: {src} to {dst}")
 
+                # Copy the data in the next file
                 shutil.copyfile(file_name, '1_' + file_name)
                 print(f"Copied: {file_name} to 1_{file_name}")
+            # Make empty file
             myfile = open(file_name, 'w')
             myfile.close()
